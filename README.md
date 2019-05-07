f773d735e74cdfd6ffd1418624d7eb719b51cd99
Blair Jordan <blair.j.jordan@gmail.com>
2019-05-07 16:55:55 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,144 @@
+# mysql-remote-multi 🐬
+
+This tool can be used to quickly retrieve dumps of MySQL databases remotely.
+
+## Usage
+
+Create **config.json** and execute `npm start`.
+
+## Demo config.json
+
+    { 
+	    "mysql": {
+		    "mysql_path" : "C:/Program Files/MySQL/MySQL Server/bin/mysql.exe",
+		    "mysqldump_path" : "C:/Program Files/MySQL/MySQL Server/bin/mysqldump.exe",
+		    "output_path" : "./output",
+		    "queries_path" : "./queries",
+		    "dump_subdir" : "dump",
+		    "results_subdir" : "results",
+        	"result_extension" : "tsv",
+			"merge_db_connection" : {
+			    "host" : "localhost",
+			    "port" : "3306",
+			    "template_db": "db_name",
+			    "database" : "merged_db",
+			    "username" : "testuser",
+			    "password" : "mypassword"
+			},
+		    "connections" : [
+			    {
+			        "host" : "EXAMPLEHOST",
+			        "port" : "3366",
+			        "database" : "db_name",
+			        "username" : "testuser",
+			        "password" : "mypassword",
+			        "sql" : ["sqlfile1", "sqlfile2"]
+			    },
+			    {
+			        "host" : "EXAMPLEHOST2",
+			        "port" : "3366",
+			        "database" : "old_db",
+			        "username" : "testuser",
+			        "password" : "mypassword",
+			        "mysql_path" : "C:/Program Files/MySQL/MySQL Server 5.0/bin/mysql.exe",
+			        "mysqldump_path" : "C:/Program Files/MySQL/MySQL Server 5.0/bin/mysqldump.exe",
+			    }
+		    ]
+	    }
+    }
+
+The demo above will connect to a single mysql host, and write dump files to **output/dump**.
+
+The following queries will also be executed:
+- queries/sqlfile1.sql
+- queries/sqlfile2.sql
+
+Query output will be saved to **output/results/db_name/**
+
+The example also shows the bin override in the EXAMPLEHOST2 connection, which allows you to use legacy binaries.
+
+## Arguments
+
+### --run-queries
+
+`node run.js --run-queries`
+
+Run all queries for each connection. Queries are specified per-connection, e.g.
+
+
+	"connections" : [
+	    {
+	        "host" : "EXAMPLEHOST",
+	        "port" : "3366",
+	        "database" : "db_name",
+	        "username" : "testuser",
+	        "password" : "mypassword",
+	        "sql" : ["sqlfile1", "sqlfile2"]
+	    },
+	    ...
+	]
+
+
+In the above example, two statements will be run: **sqlfile1.sql** and **sqlfile2.sql**.
+
+Queries are sourced from the `queries_path` folder.
+
+Results are placed under the `output_path`/`results_subdir` subdirectory.
+
+### --full-dump
+
+Export a full database dump for all database connections.
+
+`node run.js --full-dump`
+
+Dumps are placed under the `output_path`/`dump_subdir` subdirectory.
+
+### --merge
+
+Merge all databases into a single database.
+
+Before running a merge, you should ensure that you have a `merge_db_connection` object defined in **config.json**.
+
+Example:
+
+	...
+	"merge_db_connection" : {
+	    "host" : "localhost",
+	    "port" : "3306",
+	    "template_db": "db_name",
+	    "database" : "merged_db",
+	    "username" : "testuser",
+	    "password" : "mypassword"
+	},
+	"connections" : [
+        {
+	    "host" : "EXAMPLEHOST",
+	    "port" : "3366",
+	    "database" : "db_name",
+	    "username" : "testuser",
+	    "password" : "mypassword",
+	    "sql" : ["sqlfile1", "sqlfile2"]
+         },
+	...
+	]
+	...
+
+The `template_db` should refer to a specific database defined in the `connections` array.
+
+The template database is used to generate the initial schema definition.
+
+The schema definition will then be applied to databse defined in the `merge_db_connection`.
+
+### --ignore
+
+Specify tables to ignore during dump/ merge process. 
+
+`node run.js --merge --preview --ignore table1 table2`
+
+or
+
+`node run.js --merge --preview --ignore table1 --ignore table2`
+
+### -- preview
+
+Preview all mysql and mysqldump commands without running them.
---
+++ run.js
@@ -0,0 +1,200 @@
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").execSync;
+const mysqlConfig = require("./config.json").mysql;
+const replace = require('replace-in-file');
+const argv = require('yargs')
+  .version('1.0')
+  .alias('version', 'v')
+  .option('full-dump', {
+    alias: 'd',
+    describe: 'dump all databases to output folder',
+    type: 'boolean',
+    default: false
+  })
+  .option('run-queries', {
+    alias: 'q',
+    describe: 'export database query results. queries are defined in connection config(s)',
+    type: 'boolean',
+    default: false
+  })
+  .option('ignore', {
+    alias: 'i',
+    describe: 'list of tables to ignore',
+    type: 'array'
+  })
+  .option('merge', {
+    alias: 'm',
+    describe: 'merge all databases into target db',
+    type: 'boolean',
+    default: false
+  })
+  .option('preview', {
+    alias: 'p',
+    describe: 'preview commands sent to MySQL binaries',
+    type: 'boolean',
+    default: false
+  })
+  .usage('Usage: $0 <cmd> [options]')
+  .example('node run.js --full-dump --run-queries --merge', 'backup all databases, run all queries and merge all databases')
+  .argv ;
+
+if (!fs.existsSync(mysqlConfig.output_path)) {
+  fs.mkdirSync(mysqlConfig.output_path);
+}
+const preview = argv['preview'];
+const ignore = argv['ignore'];
+
+const dump = (opts) => {
+  const { mysqldump_path, host, port, database, username, password, output, input, noCreateInfo, noData, noTriggers, noRoutines, noCreateDb } = opts;  
+  const ignoreTables = (ignore && ignore.length > 0) ? ignore.map(i => `--ignore-table=${database}.${i}`).join(" ") : "";
+  const cmd = `"${mysqldump_path ? mysqldump_path : mysqlConfig.mysqldump_path}" ${ignoreTables} --host=${host} --port=${port} --user=${username} --password=${password} ${database ? '--databases ' + database : '--all-databases'} ${noTriggers ? '--skip-triggers' : ''} ${noRoutines ? '--skip-routines' : ''} ${noCreateInfo ? '--no-create-info' : ''} ${noData ? '--no-data' : ''} ${noCreateDb ? '--no-create-db' : ''} ${output ? '> ' + output : ''}  ${input ? '< ' + input : ''} --insert-ignore`;
+  
+  if (preview)
+    console.log(cmd);
+  else
+    exec(cmd);
+};
+
+const execSql = (opts) => {
+  const { host, port, database, username, password, input, output, force, mysql_path, ignoreForeignKeyChecks } = opts;
+  const cmd = `"${mysql_path ? mysql_path : mysqlConfig.mysql_path}" ${force ? '--force' : ''} ${ignoreForeignKeyChecks ? '--init-command="SET SESSION FOREIGN_KEY_CHECKS=0;"' : ''} --host=${host} --port=${port} --user=${username} --password=${password} ${database ? '--database ' + database : ''} ${input ? '< ' + input : ''} ${output ? '> ' + output : ''}`;
+  
+  if (preview)
+    console.log(cmd);
+  else
+    exec(cmd);
+};
+
+const dumpAll = () => {
+  if (!fs.existsSync(`${mysqlConfig.output_path}/${mysqlConfig.dump_subdir}`)) {
+    fs.mkdirSync(`${mysqlConfig.output_path}/${mysqlConfig.dump_subdir}`);
+  }
+  mysqlConfig.connections.forEach(c => {
+    const { host, port, database, username, password } = c;
+    try {
+      const output = `${mysqlConfig.output_path}/${mysqlConfig.dump_subdir}/${database}.sql`;
+      console.log(output);
+      dump({ host, port, database, username, password, output });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+const runQueries = () => {
+  if (!fs.existsSync(`${mysqlConfig.output_path}/${mysqlConfig.results_subdir}`)) {
+    fs.mkdirSync(`${mysqlConfig.output_path}/${mysqlConfig.results_subdir}`);
+  }
+  mysqlConfig.connections.forEach(c => {
+    const { host, port, database, username, password, mysql_path, mysqldump_path } = c;
+    
+    if (!fs.existsSync(`${mysqlConfig.output_path}/${mysqlConfig.results_subdir}/${database}`)) {
+      fs.mkdirSync(`${mysqlConfig.output_path}/${mysqlConfig.results_subdir}/${database}`);
+    }
+
+    c.sql.forEach(s => {
+      try {
+        const input = `./${mysqlConfig.queries_path}/${s}.sql`;
+        const sqlName = path.basename(input, path.extname(input));
+        const output = `${mysqlConfig.output_path}/${mysqlConfig.results_subdir}/${database}/${sqlName}.${mysqlConfig.result_extension}`;
+        console.log(output);
+        execSql({ host, port, database, username, password, input, output, mysql_path, mysqldump_path });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+};
+
+const merge = () => {
+  if (!fs.existsSync(`${mysqlConfig.output_path}/${mysqlConfig.merge_subdir}`)) {
+    fs.mkdirSync(`${mysqlConfig.output_path}/${mysqlConfig.merge_subdir}`);
+  }
+  const { merge_db_connection } = mysqlConfig;
+
+  const template = mysqlConfig.connections.filter(c => c.database === merge_db_connection.template_db).pop();
+  console.log(`Merging databases | template: ${template.database} | target:${merge_db_connection.template_db}`);
+  try {
+
+    const { host, port, database, username, password, mysql_path, mysqldump_path } = template;
+    // Generate the schema SQL
+    const sql = `${mysqlConfig.output_path}/${mysqlConfig.merge_subdir}/schema.sql`;
+    dump({
+      host,
+      port,
+      database,
+      username,
+      password,
+      output: sql,
+      noData: true,
+      noTriggers: true,
+      noRoutines: true,
+      mysql_path,
+      mysqldump_path
+    });
+    
+    replace.sync({ files: sql, from: new RegExp(template.database, "g"), to: merge_db_connection.database });
+
+    // Run the schema SQL 
+    execSql({
+      host: merge_db_connection.host,
+      port: merge_db_connection.port,
+      username: merge_db_connection.username,
+      password: merge_db_connection.password,
+      input: sql
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+  
+  mysqlConfig.connections.forEach(c => {
+    const { host, port, database, username, password, mysqldump_path } = c;
+    try {
+      const sql = `${mysqlConfig.output_path}/${mysqlConfig.merge_subdir}/${database}.sql`;
+
+      console.log(`Importing ${database} ...`);
+
+      // Generate input file for target schema
+      dump({
+        host,
+        port,
+        database,
+        username,
+        password,
+        output: sql,
+        mysqldump_path,
+        noTriggers: true,
+        noRoutines: true,
+        noCreateDb: true,
+        noCreateInfo: true
+      });
+      
+      replace.sync({ files: sql, from: /USE/g, to: '-- USE' });
+
+      // Read input file into target schema
+      execSql({
+        host: merge_db_connection.host,
+        port: merge_db_connection.port,
+        database: merge_db_connection.database,
+        username: merge_db_connection.username,
+        password: merge_db_connection.password,
+        input: sql,
+        force: false,
+        ignoreForeignKeyChecks: true
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+if (argv['full-dump'])
+  dumpAll();
+
+if (argv['run-queries'])
+  runQueries();
+  
+if (argv['merge'])
+  merge();
